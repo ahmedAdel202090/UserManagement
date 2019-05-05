780ffc94b47a0340cc2149b7584b727796e39120
MohamedSamir <m.samir@mail.com>
2019-05-05 13:00:28 +0200

    DummyBranch

--- tests/Unit/addUserTest.php
+++ tests/Unit/addUserTest.php
@@ -25,6 +25,7 @@ class addUserTest extends TestCase
         'password'=>'123456ah',
         'phone'=>'01090911402'
     ]);
+
     $mock=mockery::mock('App\Http\Controllers\UserController');
     $mock->shouldReceive('addUser')->once()->andReturn('true');
     var_dump($mock->addUser($request));
